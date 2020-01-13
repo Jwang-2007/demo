fb8d1c36de6fb14714c3b3d2b2f52b8f60f8ecf6
Jiaxing Wang <yolandwjx@gmail.com>
2020-01-12 20:47:20 -0800

    first dummy version

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
+import quandl
 
 app = Flask(__name__)
 
@@ -16,9 +17,11 @@ def about():
 
 @app.route('/render')
 def render():
+  quandl.ApiConfig.api_key = "CeGipRSorrpnDyvyQNx7"
+  data = quandl.get("WIKI/TSLA")
   plot = figure()
-  plot.circle([1, 2], [3, 4])
-  return file_html(plot, CDN, "my plot")
+  plot.circle(data.index, data.Close)
+  return file_html(plot, CDN, "TSLA Price")
 
 if __name__ == '__main__':
   app.run(port=33507)
