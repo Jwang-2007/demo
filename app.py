61130fded9dfd2515c987c54f1f0a45ad6432c7c
Jiaxing Wang <yolandwjx@gmail.com>
2020-01-12 19:46:23 -0800

    wip

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  nquestions = 5
+  return render_template('userinfo.html', num=nquestions)
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/render')
+def render():
+  plot = figure()
+  plot.circle([1, 2], [3, 4])
+  return file_html(plot, CDN, "my plot")
 
 if __name__ == '__main__':
   app.run(port=33507)
